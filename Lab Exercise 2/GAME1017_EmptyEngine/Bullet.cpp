216baea9031e44001ac638c493491cd1d60c2e66
Kraine1 <kieran.raine1@gmail.com>
2022-07-08 15:56:15 -0400

    lab 2

---
+++ Lab Exercise 2/GAME1017_EmptyEngine/Bullet.cpp
@@ -0,0 +1,18 @@
+#include "Bullet.h"
+#include "Engine.h"
+
+Bullet::Bullet(SDL_FRect dst, double dx, double dy):m_dst(dst), m_dx(dx), m_dy(dy) { }
+
+void Bullet::Update()
+{
+	m_dst.x += m_dx;
+	m_dst.y += m_dy;
+	// optional method of cleaning bullets
+	//if bullet goes off of screen
+}
+
+void Bullet::Render()
+{
+	SDL_SetRenderDrawColor(Engine::Instance().GetRenderer(), 240, 255, 0, 255);
+	SDL_RenderFillRectF(Engine::Instance().GetRenderer(), &m_dst);
+}
